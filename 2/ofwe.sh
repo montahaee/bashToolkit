06dd9f8f84af8a48bf067a65048aba369091b7cf
montaha <morteza.montahaee@outlook.com>
2024-02-09 17:57:18 +0100

    Renamed to 'ofwe.sh' and added the exception handling to the case that there is no extension to open corresponding file

--- 2/ofwe.sh
+++ 2/ofwe.sh
@@ -4,7 +4,6 @@
 directory="."
 maxdepth=""
 current=""
-desired=""
 
 # Check if the first argument is a directory
 if [ -d "$1" ]; then
@@ -19,19 +18,18 @@ while getopts "a:p:" opt; do
   case $opt in
     a) # Option -a requires an argument
       current=${OPTARG%,*} # Get the current extension before the comma
-      desired=${OPTARG#*,} # Get the desired extension after the comma
       ;;
     p) # Option -p requires an argument
       current=${OPTARG%,*} # Get the current extension before the comma
-      desired=${OPTARG#*,} # Get the desired extension after the comma
       maxdepth=1 # Change parent directory
       ;;
     *) # Invalid option
-      echo "Usage: rename_extension.sh [directory] -a|-p current,desired"
+      echo "Usage: open_extension.sh [directory] -a|-p desired_extension to open"
       exit 1
       ;;
   esac
 done
+
 
 # Invalid extension
 if [ -z "$current" ]; then
@@ -39,27 +37,35 @@ if [ -z "$current" ]; then
   exit 1
 fi
 
-if [ -z "$desired" ]; then
-  echo "Error: You've entered an empty extension for desired files."
-  exit 1
-fi
-
 # Check if the extension has a leading dot and prompt the user to remove it
-if [[ $current == .* || $desired == .* ]]; then
+if [[ $current == .* ]]; then
   echo "Warning: Please enter extensions without a leading dot (e.g., ogg instead of .ogg)"
   exit 1
 fi
 
+# Initialize an array variable
+files=()
+
 # Check if maxdepth is empty or an integer
 if [ -z "$maxdepth" ]; then
-  # Find and open files in all directories
-  find "$directory" -type f -name "*.$current" -exec bash -c 'xdg-open "$1"' _ {} \;
+  # Find and store files in all directories in the array
+  files=($(find "$directory" -type f -name "*.$current"))
 elif [ "$maxdepth" -eq "$maxdepth" ] 2>/dev/null; then
-  # Find and open files in the parent directory
-  find "$directory" -maxdepth 1 -type f -name "*.$current" -exec bash -c 'xdg-open "$1"' _ {} \;
+  # Find and store files in the parent directory in the array
+  files=($(find "$directory" -maxdepth 1 -type f -name "*.$current"))
 else
   # Invalid maxdepth value
   echo "Invalid maxdepth value: $maxdepth. Please try again."
   exit 1
+fi
+
+# Check if the array is empty and prompt the user
+if [ ${#files[@]} -eq 0 ]; then
+  echo "No files with the extension '$current' found in the directory '$directory'."
+else
+  # Loop through the array and open each file
+  for file in "${files[@]}"; do
+    xdg-open "$file"
+  done
 fi
 
